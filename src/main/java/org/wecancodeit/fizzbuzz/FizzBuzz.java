c13315377ae9eb4231a5019ec31f668928864e5c
Fatuma Pemba <fatuma.pemba@yahoo.com>
2020-01-18 20:58:29 -0500

    added last test

--- src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,20 @@ package org.wecancodeit.fizzbuzz;
 
 
 public class FizzBuzz {
+    public String interpret(int numberToInterpret) {
+
+        if((numberToInterpret % 3 == 0) && (numberToInterpret % 5 == 0)) {
+            return "FizzBuzz";
+        }
+        if(numberToInterpret % 3 ==0){
+            return "Fizz";
+        }
+        if(numberToInterpret % 5 == 0){
+            return "Buzz";
+        }
+        {
+            return "" + numberToInterpret;
+        }
+
+    }
 }
